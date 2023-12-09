a2467fa70577f918b4e6abfb4ec7d0f5e4ff1768
Matcon12 <147035101+Matcon12@users.noreply.github.com>
2023-12-09 16:51:02 +0530

    Update views.py

--- server/views.py
+++ server/views.py
@@ -198,12 +198,11 @@ class GetPartNameView(APIView):
 class GetPODetailsView(APIView):
     def get(self, request, po_no):
         try:
-            # po_instance = Po.objects.filter(po_no=po_no).first()
-            po_instance =get_object_or_404(Po,po_no=po_no)[0]
+            po_instance = Po.objects.filter(po_no=po_no).first()
             print(po_instance,"po no")
 
             if po_instance:
-                serializer = POSerializer(po_instance)
+                serializer = PurchaseOrderForm(po_instance)
                 print(serializer,"serializer")
                 response_data={
                 'po_date': serializer.data['po_date'],
